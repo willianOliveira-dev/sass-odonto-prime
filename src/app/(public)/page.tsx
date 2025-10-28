16596651c1873e2627db22b7df0cee47a7462012
Willian Oliveira <willian.dev.tech@gmail.com>
2025-10-28 19:37:05 -0300

    fix: solve the problem of fetching data persisting in the cache

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,11 @@
-'use server';
-import { Header } from './_components/_Header';
-import { Hero } from './_components/_Hero';
-import { Clinic } from './_components/_Clinic';
-import { Professionals } from './_components/_Professionals';
-import { Footer } from './_components/_Footer';
-import { getProfessionals } from './data-access-layer/getProfessionals';
+import { Header } from "./_components/_Header";
+import { Hero } from "./_components/_Hero";
+import { Clinic } from "./_components/_Clinic";
+import { Professionals } from "./_components/_Professionals";
+import { Footer } from "./_components/_Footer";
+import { getProfessionals } from "./data-access-layer/getProfessionals";
+
+export const revalidate = 90;
 
 export default async function HomePage() {
     const professionals = await getProfessionals();
@@ -13,7 +14,7 @@ export default async function HomePage() {
             <Header />
             <Hero />
             <Clinic />
-            <Professionals  professionals={professionals || []}/>
+            <Professionals professionals={professionals || []} />
             <Footer />
         </div>
     );
