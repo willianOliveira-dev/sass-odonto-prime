ea10d60831e907fcafb2912181660c3926b63f73
Willian Oliveira <willian.dev.tech@gmail.com>
2025-10-30 15:26:29 -0300

    fix(auth): update credentials login flow to handle manual redirect and error handling

--- src/app/(auth)/(login)/_actions/loginActionProvider.ts
+++ src/app/(auth)/(login)/_actions/loginActionProvider.ts
@@ -1,8 +1,11 @@
-'use client';
-import { signIn } from 'next-auth/react';
+"use client";
+import { signIn } from "next-auth/react";
 
-type LoginActionProviderProps = 'google' | 'facebook';
+type LoginActionProviderProps = "google" | "facebook";
 
 export async function loginActionProvider(provider: LoginActionProviderProps) {
-    await signIn(provider, { redirectTo: '/dashboard' });
+  const result = await signIn(provider, { callbackUrl: "/dashboard" });
+  if (result?.error) {
+    console.log(result.error);
+  }
 }
